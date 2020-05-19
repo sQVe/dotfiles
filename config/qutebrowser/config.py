54d858ee9b065a50daeb5f4c82c066bf73f48e05
Oskar Grunning <oskargrunning@gmail.com>
2020-05-19 09:36:54 +0200

    Properly setup font families and emoji support
    Reference: https://github.com/alacritty/alacritty/issues/153

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1313,7 +1313,7 @@ c.colors.webpage.prefers_color_scheme_dark = True
 # font setting, it's replaced with the fonts listed here. If set to an
 # empty value, a system-specific monospace default is used.
 # Type: List of Font, or Font
-c.fonts.default_family = ['Iosevka Nerd Font Mono', 'monospace']
+c.fonts.default_family = ['term']
 
 # Default font size to use. Whenever "default_size" is used in a font
 # setting, it's replaced with the size listed here. Valid values are
@@ -1372,19 +1372,19 @@ c.fonts.tabs = '12px default_family'
 
 # Font family for standard fonts.
 # Type: FontFamily
-c.fonts.web.family.standard = 'Arimo'
+c.fonts.web.family.standard = 'san-serif'
 
 # Font family for fixed fonts.
 # Type: FontFamily
-c.fonts.web.family.fixed = 'Cousine'
+c.fonts.web.family.fixed = 'monospace'
 
 # Font family for serif fonts.
 # Type: FontFamily
-c.fonts.web.family.serif = 'Tinos'
+c.fonts.web.family.serif = 'serif'
 
 # Font family for sans-serif fonts.
 # Type: FontFamily
-c.fonts.web.family.sans_serif = 'Arimo'
+c.fonts.web.family.sans_serif = 'sans-serif'
 
 # Font family for cursive fonts.
 # Type: FontFamily
