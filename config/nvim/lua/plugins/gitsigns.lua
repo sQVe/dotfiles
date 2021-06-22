a45d26531765b63850b80873072cbf818ddfb531
Oskar Grunning <oskargrunning@gmail.com>
2021-06-22 08:55:33 +0200

    chore: finish gitsigns settings

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -1,33 +1,61 @@
 --  ┏━╸╻╺┳╸┏━┓╻┏━╸┏┓╻┏━┓
 --  ┃╺┓┃ ┃ ┗━┓┃┃╺┓┃┗┫┗━┓
 --  ┗━┛╹ ╹ ┗━┛╹┗━┛╹ ╹┗━┛
--- Highlight
--- vim.cmd [[
--- 'hi! GitSignsAdd guifg=#b8bb26 guibg=NONE gui=NONE cterm=NONE'
--- 'hi! GitSignsChange guifg=#d3869b guibg=NONE gui=NONE cterm=NONE'
--- hi! GitSignsDelete guifg=#fb4934 guibg=NONE gui=NONE cterm=NONE
--- ]]
-require('gitsigns').setup({
-    keymaps = {
-        noremap = true,
-        buffer = true,
+return function()
+    require('gitsigns').setup({
+        signs = {
+            add = {
+                hl = 'GruvboxGreen',
+                text = '│',
+                numhl = 'GitSignsAddNr',
+                linehl = 'GitSignsAddLn'
+            },
+            change = {
+                hl = 'GruvboxAqua',
+                text = '│',
+                numhl = 'GitSignsChangeNr',
+                linehl = 'GitSignsChangeLn'
+            },
+            delete = {
+                hl = 'GruvboxRed',
+                text = '│',
+                numhl = 'GitSignsDeleteNr',
+                linehl = 'GitSignsDeleteLn'
+            },
+            topdelete = {
+                hl = 'GruvboxRed',
+                text = '│',
+                numhl = 'GitSignsDeleteNr',
+                linehl = 'GitSignsDeleteLn'
+            },
+            changedelete = {
+                hl = 'GruvboxAqua',
+                text = '│',
+                numhl = 'GitSignsChangeNr',
+                linehl = 'GitSignsChangeLn'
+            }
+        },
+        keymaps = {
+            noremap = true,
+            buffer = true,
 
-        ['n <leader>hj'] = {
-            expr = true,
-            "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"
-        },
-        ['n <leader>hk'] = {
-            expr = true,
-            "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"
-        },
-        ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-        ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-        ['n <leader>hi'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-        ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-        ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
-    }
-})
+            ['n <leader>hj'] = {
+                expr = true,
+                "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"
+            },
+            ['n <leader>hk'] = {
+                expr = true,
+                "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"
+            },
+            ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+            ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+            ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+            ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+            ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+            ['n <leader>hi'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+            ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+            ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+            ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+        }
+    })
+end
