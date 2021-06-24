ae199110cea21d7cef7e551a9351bc90e01a8cc7
Oskar Grunning <oskargrunning@gmail.com>
2021-06-24 23:06:52 +0200

    chore: set filetype overrides for vsnip

--- config/nvim/lua/plugins/snippets/vsnip.lua
+++ config/nvim/lua/plugins/snippets/vsnip.lua
@@ -4,11 +4,15 @@
 return function()
     vim.cmd([[
       let g:vsnip_snippet_dir = expand('$XDG_CONFIG_HOME/nvim/snippets')
+      let g:vsnip_filetypes = {}
+      let g:vsnip_filetypes.javascriptreact = ['javascript', 'react']
+      let g:vsnip_filetypes.typescript = ['javascript', ]
+      let g:vsnip_filetypes.typescriptreact = ['javascript', 'react', 'typescript']
     ]])
 
     -- Expand or jump.
     vim.cmd([[
       imap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
       smap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'
-  ]])
+    ]])
 end
