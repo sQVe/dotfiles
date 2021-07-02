280cd067f5d73bb319bc2285584d78270c7b00a1
Oskar Grunning <oskargrunning@gmail.com>
2021-07-02 08:41:23 +0200

    chore: simplify compe keybind functions

--- config/nvim/lua/plugins/lsp/compe.lua
+++ config/nvim/lua/plugins/lsp/compe.lua
@@ -19,17 +19,13 @@ return function()
 
     local check_back_space = function()
         local col = vim.fn.col('.') - 1
-        if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-            return true
-        else
-            return false
-        end
+        return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
     end
 
     _G.tab_complete = function()
         if vim.fn.pumvisible() == 1 then
             return t "<C-n>"
-        elseif vim.fn.call("vsnip#available", {1}) == 1 then
+        elseif vim.fn['vsnip#available'](1) == 1 then
             return t "<Plug>(vsnip-expand-or-jump)"
         elseif check_back_space() then
             return t "<Tab>"
@@ -40,7 +36,7 @@ return function()
     _G.s_tab_complete = function()
         if vim.fn.pumvisible() == 1 then
             return t "<C-p>"
-        elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+        elseif vim.fn['vsnip#jumpable'](-1) == 1 then
             return t "<Plug>(vsnip-jump-prev)"
         else
             return t "<S-Tab>"
