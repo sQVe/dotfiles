0294bacfce2c94716e328bb64fc77c4a58db5ab7
Oskar Grunning <oskargrunning@gmail.com>
2021-08-25 11:57:01 +0200

    chore: add typescript to node default packages

--- config/nvim/lua/main.lua
+++ config/nvim/lua/main.lua
@@ -7,9 +7,7 @@ vim.g.loaded_getscript = 1
 vim.g.loaded_getscriptPlugin = 1
 vim.g.loaded_gzip = 1
 vim.g.loaded_logiPat = 1
-vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
-vim.g.loaded_netrw = 1
 vim.g.loaded_netrwFileHandlers = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.loaded_netrwSettings = 1
@@ -20,5 +18,9 @@ vim.g.loaded_vimball = 1
 vim.g.loaded_vimballPlugin = 1
 vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
+
+-- Netrw and matchit used by fugitive.
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_matchit = 1
 
 require('plugins')
