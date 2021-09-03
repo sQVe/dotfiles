4d5cfebf275fe47ad42ecc4a9b57803345309591
Oskar Grunning <oskargrunning@gmail.com>
2021-09-03 20:31:16 +0200

    chore: add border to lsp signature

--- config/nvim/lua/plugins/lsp/signature.lua
+++ config/nvim/lua/plugins/lsp/signature.lua
@@ -3,7 +3,9 @@
 --  ┗━┛╹┗━┛╹ ╹╹ ╹ ╹ ┗━┛╹┗╸┗━╸
 return function()
     require'lsp_signature'.on_attach({
+        bind = true,
         floating_window = true,
+        handler_opts = {border = "single"},
         hint_enable = false
     })
 end
