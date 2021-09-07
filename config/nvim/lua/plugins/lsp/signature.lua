60ecafea01c216c208b9d1e2df0bddba4f55970e
Oskar Grunning <oskargrunning@gmail.com>
2021-09-07 10:55:52 +0200

    chore: fix signature index and positioning

--- config/nvim/lua/plugins/lsp/signature.lua
+++ config/nvim/lua/plugins/lsp/signature.lua
@@ -5,8 +5,9 @@ return function()
     require'lsp_signature'.on_attach({
         bind = true,
         floating_window = true,
+        floating_window_above_cur_line = true,
         handler_opts = {border = "single"},
         hint_enable = false,
-        zindex = 10
+        zindex = 50
     })
 end
