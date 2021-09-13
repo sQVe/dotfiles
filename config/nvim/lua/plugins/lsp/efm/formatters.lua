9b2488f07a0cd2238e358f9802471079c528feaf
Oskar Grunning <oskargrunning@gmail.com>
2021-09-13 16:28:59 +0200

    chore: use prettierd and only local version of prettier

--- config/nvim/lua/plugins/lsp/efm/formatters.lua
+++ config/nvim/lua/plugins/lsp/efm/formatters.lua
@@ -5,8 +5,9 @@ return {
     gofmt = {formatCommand = "gofmt", formatStdin = true},
     luaformat = {formatCommand = "lua-format -i", formatStdin = true},
     prettier = {
-        formatCommand = "node /home/sqve/code/prettier_d_slim/lib/bin/prettier_d_slim.js --stdin --stdin-filepath ${INPUT}",
-        formatStdin = true
+        formatCommand = 'prettierd "${INPUT}"',
+        formatStdin = true,
+        env = {"PRETTIERD_LOCAL_PRETTIER_ONLY=true"}
     },
     shfmt = {formatCommand = 'shfmt -i 2 -bn -ci -sr', formatStdin = true}
 }
