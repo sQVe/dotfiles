2581362637d41e52b2b93a2b74736a8429185c7f
Oskar Grunning <oskargrunning@gmail.com>
2021-09-27 08:01:06 +0200

    chore: handle , and ; for lightspeed

--- config/nvim/lua/plugins/navigation/lightspeed.lua
+++ config/nvim/lua/plugins/navigation/lightspeed.lua
@@ -7,4 +7,11 @@ return function()
         cycle_group_fwd_key = '<Tab>',
         jump_to_first_match = false
     }
+
+    vim.cmd([[
+        map , <Plug>Lightspeed_;_sx
+        map ; <Plug>Lightspeed_,_sx
+        map , <Plug>Lightspeed_;_ft
+        map ; <Plug>Lightspeed_,_ft
+    ]])
 end
