da254384f26e96ac2267da73b98290d5e31e2726
Oskar Grunning <oskargrunning@gmail.com>
2021-11-04 06:55:47 +0100

    chore: calculate commentstring location

--- config/nvim/lua/plugins/documentation/comment.lua
+++ config/nvim/lua/plugins/documentation/comment.lua
@@ -9,10 +9,20 @@ return function()
 
             local type = ctx.ctype == utils.ctype.line and '__default' or
                              '__multiline'
+
+            local location = nil
+            if ctx.ctype == utils.ctype.block then
+                location =
+                    require('ts_context_commentstring.utils').get_cursor_location()
+            elseif ctx.cmotion == utils.cmotion.v or ctx.cmotion ==
+                utils.cmotion.V then
+                location =
+                    require('ts_context_commentstring.utils').get_visual_start_location()
+            end
+
             return
-                require('ts_context_commentstring.internal').calculate_commentstring {
-                    key = type,
-                }
+                require('ts_context_commentstring.internal').calculate_commentstring(
+                    {key = type, location = location})
         end,
     })
 end
