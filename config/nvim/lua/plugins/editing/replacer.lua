23ce1139133954c5459e5da64107a773492c6d0a
Oskar Grunning <oskargrunning@gmail.com>
2021-11-07 15:07:13 +0100

    chore: add replace content command

--- config/nvim/lua/plugins/editing/replacer.lua
+++ config/nvim/lua/plugins/editing/replacer.lua
@@ -4,5 +4,6 @@
 return function()
     vim.cmd([[
         command! Replace lua require("replacer").run()<CR>
+        command! ReplaceContent lua require("replacer").run({ rename_files = false })<CR>
     ]])
 end
