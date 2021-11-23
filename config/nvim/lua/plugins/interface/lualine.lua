71a0ab31e59334518746c53245e766d4df2fc0d9
Oskar Grunning <oskargrunning@gmail.com>
2021-11-23 23:12:00 +0100

    chore: tweak lualine further

--- config/nvim/lua/plugins/interface/lualine.lua
+++ config/nvim/lua/plugins/interface/lualine.lua
@@ -3,11 +3,21 @@
 --  ┗━╸┗━┛╹ ╹┗━╸╹╹ ╹┗━╸
 return function()
     local gruvbox = require('lualine.themes.gruvbox')
+    local modes = {
+        'command', 'inactive', 'insert', 'normal', 'replace', 'visual',
+    }
 
-    gruvbox.normal.c.gui = 'italic'
+    for _, mode in pairs(modes) do
+        gruvbox[mode].c.gui = 'italic'
+    end
 
     require('lualine').setup {
-        options = {theme = gruvbox, disabled_filetypes = {'alpha'}},
+        options = {
+            component_separators = {'', ''},
+            section_separators = {'', ''},
+            disabled_filetypes = {'alpha'},
+            theme = gruvbox,
+        },
         sections = {
             lualine_a = {'mode'},
             lualine_b = {'filename'},
@@ -20,9 +30,9 @@ return function()
             lualine_a = {},
             lualine_b = {},
             lualine_c = {},
-            lualine_x = {'filename'},
+            lualine_x = {},
             lualine_y = {},
-            lualine_z = {},
+            lualine_z = {'filename'},
         },
     }
 end
