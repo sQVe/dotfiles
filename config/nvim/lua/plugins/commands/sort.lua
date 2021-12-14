012caae28ae1afbea2b748bcc186a7cac52ca3b3
Oskar Grunning <oskargrunning@gmail.com>
2021-12-14 21:22:59 +0100

    chore: add more sort keybindings

--- config/nvim/lua/plugins/commands/sort.lua
+++ config/nvim/lua/plugins/commands/sort.lua
@@ -5,6 +5,12 @@
 return function()
   vim.cmd([[
     nnoremap <silent> go <Cmd>Sort<CR>
+    nnoremap <silent> go" vi"<Esc><Cmd>Sort<CR>
+    nnoremap <silent> go' vi'<Esc><Cmd>Sort<CR>
+    nnoremap <silent> go( vi(<Esc><Cmd>Sort<CR>
+    nnoremap <silent> go[ vi[<Esc><Cmd>Sort<CR>
+    nnoremap <silent> gop vip<Esc><Cmd>Sort<CR>
+    nnoremap <silent> go{ vi{<Esc><Cmd>Sort<CR>
     vnoremap <silent> go <Esc><Cmd>Sort<CR>
   ]])
 end
