b0fc925c862059fdc275d1b5210f3f7d36743982
Oskar Grunning <oskargrunning@gmail.com>
2021-12-30 09:41:31 +0100

    chore: use runtime_condition for null-ls

--- config/nvim/lua/plugins/lsp/null.lua
+++ config/nvim/lua/plugins/lsp/null.lua
@@ -10,6 +10,39 @@ return function()
   local formatters = null.builtins.formatting
   local linters = null.builtins.diagnostics
 
+  local eslint_config_cache = {}
+  local eslint_runtime_condition = function(params)
+    if eslint_config_cache[params.bufnr] ~= nil then
+      return eslint_config_cache[params.bufnr]
+    end
+
+    local has_config = require('lspconfig').util.root_pattern({
+      '.eslintrc.cjs',
+      '.eslintrc.js',
+      '.eslintrc.json',
+      '.eslintrc.yaml',
+      '.eslintrc.yml',
+    })(params.bufname) ~= nil
+    eslint_config_cache[params.bufnr] = has_config
+
+    return has_config
+  end
+
+  local stylua_config_cache = {}
+  local stylua_runtime_condition = function(params)
+    if stylua_config_cache[params.bufnr] ~= nil then
+      return stylua_config_cache[params.bufnr]
+    end
+
+    local has_config = require('lspconfig').util.root_pattern({
+      'stylua.toml',
+      '.stylua.toml',
+    })(params.bufname) ~= nil
+    stylua_config_cache[params.bufnr] = has_config
+
+    return has_config
+  end
+
   null.setup({
     cmd = { 'nvim' },
     diagnostics_format = '#{c}: #{m} (#{s})',
@@ -31,21 +64,9 @@ return function()
       formatters.shfmt.with({
         extra_args = { '-i', '2', '-bn', '-ci', '-sr' },
       }),
-      formatters.stylua.with({
-        condition = function(utils)
-          return utils.root_has_file({ 'stylua.toml', '.stylua.toml' })
-        end,
-      }),
+      formatters.stylua.with({ runtime_condition = stylua_runtime_condition }),
       linters.eslint_d.with({
-        condition = function(utils)
-          return utils.root_has_file({
-            '.eslintrc.cjs',
-            '.eslintrc.js',
-            '.eslintrc.json',
-            '.eslintrc.yaml',
-            '.eslintrc.yml',
-          })
-        end,
+        runtime_condition = eslint_runtime_condition,
         timeout = 20000,
       }),
       linters.shellcheck,
