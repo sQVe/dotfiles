d145168fb3c3982d1e26032ff4249895a909993c
Oskar Grunning <oskargrunning@gmail.com>
2022-02-07 14:53:06 +0100

    chore: tweak monospace alias font and setup

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1364,7 +1364,7 @@ c.colors.webpage.preferred_color_scheme = 'dark'
 # font setting, it's replaced with the fonts listed here. If set to an
 # empty value, a system-specific monospace default is used.
 # Type: List of Font, or Font
-c.fonts.default_family = ['term']
+c.fonts.default_family = ['monospace']
 
 # Default font size to use. Whenever "default_size" is used in a font
 # setting, it's replaced with the size listed here. Valid values are
