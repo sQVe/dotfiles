e3de5d70084e599d4ba0681bdd68430e736c63b9
Oskar Grunning <oskargrunning@gmail.com>
2022-06-09 20:09:21 +0200

    chore: split hook snippets into own collection

--- config/nvim/lua/plugins/vsnip.lua
+++ config/nvim/lua/plugins/vsnip.lua
@@ -6,9 +6,10 @@ return function()
   vim.cmd([[
     let g:vsnip_snippet_dir = expand('$XDG_CONFIG_HOME/nvim/snippets')
     let g:vsnip_filetypes = {}
-    let g:vsnip_filetypes.javascriptreact = ['javascript']
-    let g:vsnip_filetypes.typescript = ['javascript']
-    let g:vsnip_filetypes.typescriptreact = ['javascript', 'javascriptreact', 'typescript']
+    let g:vsnip_filetypes.javascript = ['hooks']
+    let g:vsnip_filetypes.javascriptreact = ['javascript', 'hooks']
+    let g:vsnip_filetypes.typescript = ['javascript', 'hooks']
+    let g:vsnip_filetypes.typescriptreact = ['javascript', 'javascriptreact', 'typescript', 'hooks']
   ]])
 
   -- Expand or jump.
