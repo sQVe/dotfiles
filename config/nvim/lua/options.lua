c0b54b32a4775db6a43b3960315c3ba7af303762
Oskar Grunning <oskargrunning@gmail.com>
2022-08-15 14:44:25 +0200

    chore: hide cmd again

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -5,7 +5,7 @@
 -- stylua: ignore
 local options = {
   clipboard = 'unnamedplus',                        -- Use the + (clipboard) register.
-  -- cmdheight = 0,                                    -- Hide command-line when possible.
+  cmdheight = 0,                                    -- Hide command-line when possible.
   conceallevel = 0,                                 -- Disable conceal.
   exrc = true,                                      -- Source local vimrc at root directory.
   gdefault = true,                                  -- Use 'g' flag by default with :s/foo/bar/.
