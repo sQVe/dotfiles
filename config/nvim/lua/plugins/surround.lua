b22c73488221474032b95a8ffd7dd067168cae34
Oskar Grunning <oskargrunning@gmail.com>
2022-08-15 09:23:22 +0200

    chore: tweak nvim surround config

--- config/nvim/lua/plugins/surround.lua
+++ config/nvim/lua/plugins/surround.lua
@@ -3,43 +3,49 @@
 -- ┗━┛┗━┛╹┗╸╹┗╸┗━┛┗━┛╹ ╹╺┻┛
 
 return function()
-  local get_input = require('nvim-surround.utils').get_input
+  local get_selection = require('nvim-surround.utils').config
 
   require('nvim-surround').setup({
     keymaps = { visual = 'gs' },
-    delimiters = {
-      pairs = {
-        ['('] = { '(', ')' },
-        ['<'] = { '<', '>' },
-        ['['] = { '[', ']' },
-        ['{'] = { '{', '}' },
-        ['a'] = function()
-          local head = get_input('Enter mirror pair: ')
-          local tail = string.reverse(string.gsub(head, '.', {
-            ['('] = ')',
-            ['<'] = '>',
-            ['['] = ']',
-            ['{'] = '}',
-          }))
-
-          return { head, tail }
+    surrounds = {
+      ['('] = {
+        add = { '(', ')' },
+        find = function()
+          return get_selection({ textobject = '(' })
         end,
-        ['f'] = function()
-          return { get_input('Enter function name: ') .. '(', ')' }
-        end,
-        ['i'] = function()
-          local head = get_input('Enter head: ')
-          local tail = get_input('Enter tail: ')
-
-          return { head, string.len(tail) > 0 and tail or head }
-        end,
+        delete = '^(. ?)().-( ?.)()$',
+        change = { target = '^(. ?)().-( ?.)()$' },
       },
-      aliases = {
-        ['a'] = false,
-        ['b'] = false,
-        ['B'] = false,
-        ['r'] = false,
+      ['{'] = {
+        add = { '{', '}' },
+        find = function()
+          return get_selection({ textobject = '{' })
+        end,
+        delete = '^(. ?)().-( ?.)()$',
+        change = { target = '^(. ?)().-( ?.)()$' },
       },
+      ['<'] = {
+        add = { '<', '>' },
+        find = function()
+          return get_selection({ textobject = '<' })
+        end,
+        delete = '^(. ?)().-( ?.)()$',
+        change = { target = '^(. ?)().-( ?.)()$' },
+      },
+      ['['] = {
+        add = { '[', ']' },
+        find = function()
+          return get_selection({ textobject = '[' })
+        end,
+        delete = '^(. ?)().-( ?.)()$',
+        change = { target = '^(. ?)().-( ?.)()$' },
+      },
+    },
+    aliases = {
+      ['a'] = false,
+      ['b'] = false,
+      ['B'] = false,
+      ['r'] = false,
     },
   })
 end
