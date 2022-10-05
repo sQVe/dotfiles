49b296f22bdf8bba9ff3cf67294b2f24445c6b08
Oskar Grunning <oskargrunning@gmail.com>
2022-10-05 11:44:46 +0200

    chore: repair leap settings

--- config/nvim/lua/plugins/leap.lua
+++ config/nvim/lua/plugins/leap.lua
@@ -7,14 +7,13 @@ return function()
 
   leap.set_default_keymaps()
   leap.setup({
-    highlight_ahead_of_time = false,
     special_keys = {
       repeat_search = '<CR>',
-      next_match = '<CR>',
-      prev_match = '<S-CR>',
+      next_aot_match = '<CR>',
+      next_match = { ';', '<CR>' },
+      prev_match = { ',', '<S-CR>' },
       next_group = '<Tab>',
       prev_group = '<S-Tab>',
-      eol = '<Space>',
     },
   })
 end
