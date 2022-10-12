55d3b4fcf1d90d9e97583a31d850fc5d1417dd1d
Oskar Grunning <oskargrunning@gmail.com>
2022-10-12 12:32:36 +0200

    chore: invert next and prev leap match , / ;

--- config/nvim/lua/plugins/leap.lua
+++ config/nvim/lua/plugins/leap.lua
@@ -10,8 +10,8 @@ return function()
     special_keys = {
       repeat_search = '<CR>',
       next_aot_match = '<CR>',
-      next_match = { ';', '<CR>' },
-      prev_match = { ',', '<S-CR>' },
+      next_match = { ',', '<CR>' },
+      prev_match = { ';', '<S-CR>' },
       next_group = '<Tab>',
       prev_group = '<S-Tab>',
     },
