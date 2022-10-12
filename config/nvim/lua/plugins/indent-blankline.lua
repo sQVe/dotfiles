5774e39477323e9cabe349d4f61e3388e40d8430
Oskar Grunning <oskargrunning@gmail.com>
2022-10-12 12:07:39 +0200

    chore: update indent_blankline setup

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -3,36 +3,10 @@
 --  ╹╹ ╹╺┻┛┗━╸╹ ╹ ╹    ┗━┛┗━╸╹ ╹╹ ╹╹ ╹┗━╸╹╹ ╹┗━╸
 
 return function()
-  vim.g.indent_blankline_char = '│'
-  vim.g.indent_blankline_filetype_exclude = { 'alpha', 'help', 'packer' }
-  vim.g.indent_blankline_show_current_context = true
-  vim.g.indent_blankline_show_first_indent_level = false
-  vim.g.indent_blankline_use_treesitter = true
-
-  vim.g.indent_blankline_context_patterns = {
-    -- JavaScript and TypeScript
-    'declaration',
-    'expression',
-    'pattern',
-    'statement',
-    'switch_case',
-    'switch_default',
-    'jsx_attribute',
-    'jsx_element',
-    'jsx_fragment',
-    'jsx_self_closing_element',
-
-    -- Bash and Go.
-    '_expression',
-    '_simple_statement',
-    '_simple_type',
-    '_statement',
-    '_type',
-
-    -- General.
-    'class',
-    'function',
-    'method',
-    'pair',
-  }
+  require('indent_blankline').setup({
+    filetype_exclude = { 'alpha', 'help', 'packer' },
+    buftype_exclude = { 'nofile', 'terminal' },
+    show_current_context = true,
+    use_treesitter = true,
+  })
 end
