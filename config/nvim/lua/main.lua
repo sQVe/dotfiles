b8253bed2c2688d9faac52de7c09c52ac2fe10e0
Oskar Grunning <oskargrunning@gmail.com>
2022-10-13 22:14:51 +0200

    chore: add todo comments

--- config/nvim/lua/main.lua
+++ config/nvim/lua/main.lua
@@ -2,6 +2,14 @@
 --  ┃┃┃┣━┫┃┃┗┫
 --  ╹ ╹╹ ╹╹╹ ╹
 
+-- TODO:
+-- Move abbrevations into lua module.
+-- Convert functions into lua.
+--   Where should these be housed? functions.lua, a util or what?
+-- Convert keymaps into lua.
+-- Move everything under `lua/sQVe` scope.
+-- Create init.lua
+
 require('disable')
 require('autocmds')
 require('options')
