8a0554441c464546c6d89a91c960d6fa55960fdc
Oskar Grunning <oskargrunning@gmail.com>
2022-11-18 15:34:53 +0100

    chore: make tree properly async

--- config/nvim/lua/sQVe/plugins/tree.lua
+++ config/nvim/lua/sQVe/plugins/tree.lua
@@ -8,7 +8,7 @@ local M = {}
 M.init = function(use)
   use({
     'nvim-tree/nvim-tree.lua',
-    commands = { 'NvimTreeToggle' },
+    cmd = { 'NvimTreeToggle' },
     config = M.config,
     setup = M.setup,
     tag = 'nightly',
