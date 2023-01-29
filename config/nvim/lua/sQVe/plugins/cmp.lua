b72e18c17ab2ed083a8e8e87ad253ca26611b330
Oskar Grunning <oskargrunning@gmail.com>
2023-01-29 14:20:17 +0100

    chore: do not expand luasnips with tab

--- config/nvim/lua/sQVe/plugins/cmp.lua
+++ config/nvim/lua/sQVe/plugins/cmp.lua
@@ -57,8 +57,8 @@ M.config = function()
   local next = function(fallback)
     if cmp.visible() then
       cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
-      luasnip.expand_or_jump()
+    elseif luasnip.jumpable(1) then
+      luasnip.jump(1)
     elseif has_words_before() then
       cmp.complete()
     else
