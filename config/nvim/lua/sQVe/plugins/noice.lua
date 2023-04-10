9da993659dfb2fc28a93ea3305ff27dd68af4492
Oskar Grunning <oskargrunning@gmail.com>
2023-04-10 18:29:13 +0200

    fix: add more messages to ignore

--- config/nvim/lua/sQVe/plugins/noice.lua
+++ config/nvim/lua/sQVe/plugins/noice.lua
@@ -49,9 +49,38 @@ M.opts = {
       filter = { event = 'msg_show', min_height = 4 },
       view = 'popup',
     },
+
     -- Skip any write message.
     {
       filter = { event = 'msg_show', find = '%d+L, %d+B$' },
+      opts = { skip = true },
+    },
+
+    -- Skip any "fewer lines" or "more lines" messages.
+    {
+      filter = { event = 'msg_show', find = '^%d+ fewer lines' },
+      opts = { skip = true },
+    },
+    {
+      filter = { event = 'msg_show', find = '^%d+ line less' },
+      opts = { skip = true },
+    },
+    {
+      filter = { event = 'msg_show', find = '^%d+ more lines?' },
+      opts = { skip = true },
+    },
+
+    -- Skip any undo and redo messages.
+    {
+      filter = { event = 'msg_show', find = '^%d+ changes?; before' },
+      opts = { skip = true },
+    },
+    {
+      filter = { event = 'msg_show', find = '^%d+ changes?; after' },
+      opts = { skip = true },
+    },
+    {
+      filter = { event = 'msg_show', find = '^Already at newest change$' },
       opts = { skip = true },
     },
   },
