81662b8ef904674fa3370202bf67be613f2e50f1
Oskar Grunning <oskargrunning@gmail.com>
2023-04-12 13:01:25 +0200

    chore: repair comment setup

--- config/nvim/lua/sQVe/plugins/comment.lua
+++ config/nvim/lua/sQVe/plugins/comment.lua
@@ -12,9 +12,12 @@ local M = {
   dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
 }
 
-M.opts = {
-  ignore = '^$',
-  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-}
+-- Set opts via a function since we're running a require in it.
+M.opts = function()
+  return {
+    ignore = '^$',
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+  }
+end
 
 return M
