64a1f7b6fcfbebf2630201da781043f5e278d945
Oskar Grunning <oskargrunning@gmail.com>
2023-04-17 11:05:52 +0200

    chore: tweak diagnostics signs

--- config/nvim/lua/sQVe/plugins/gruvbox.lua
+++ config/nvim/lua/sQVe/plugins/gruvbox.lua
@@ -106,19 +106,19 @@ M.config = function(_, opts)
   -- Set diagnostic signs.
   vim.fn.sign_define(
     'DiagnosticSignError',
-    { text = '', texthl = 'DiagnosticSignError' }
+    { text = '', texthl = 'DiagnosticSignError' }
   )
   vim.fn.sign_define(
     'DiagnosticSignHint',
-    { text = '', texthl = 'DiagnosticSignHint' }
+    { text = '', texthl = 'DiagnosticSignHint' }
   )
   vim.fn.sign_define(
     'DiagnosticSignInfo',
-    { text = '', texthl = 'DiagnosticSignInfo' }
+    { text = '', texthl = 'DiagnosticSignInfo' }
   )
   vim.fn.sign_define(
     'DiagnosticSignWarn',
-    { text = '', texthl = 'DiagnosticSignWarn' }
+    { text = '', texthl = 'DiagnosticSignWarn' }
   )
 
   vim.o.background = 'dark'
