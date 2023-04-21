aa4d8b45fb8d07e263436d5fd5ef7e54a4668029
Oskar Grunning <oskargrunning@gmail.com>
2023-04-21 23:06:14 +0200

    chore: setup nvim as mergetool

---
+++ config/nvim/lua/sQVe/plugins/git-conflict.lua
@@ -0,0 +1,14 @@
+-- ┏━╸╻╺┳╸   ┏━╸┏━┓┏┓╻┏━╸╻  ╻┏━╸╺┳╸
+-- ┃╺┓┃ ┃    ┃  ┃ ┃┃┗┫┣╸ ┃  ┃┃   ┃
+-- ┗━┛╹ ╹    ┗━╸┗━┛╹ ╹╹  ┗━╸╹┗━╸ ╹
+-- Visualize and resolve merge conflicts.
+
+local M = {
+  'akinsho/git-conflict.nvim',
+  version = '*',
+  event = 'BufRead',
+}
+
+M.config = true
+
+return M
