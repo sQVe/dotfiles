b79e0d9cf765eec952da016c9a6f1ac216c0cdd7
Oskar Grunning <oskargrunning@gmail.com>
2023-10-07 21:08:29 +0200

    chore: add luacheck linter

--- config/nvim/lua/sQVe/plugins/lint.lua
+++ config/nvim/lua/sQVe/plugins/lint.lua
@@ -21,6 +21,7 @@ M.opts = {
   linters_by_ft = {
     javascript = { 'eslint_d' },
     javascriptreact = { 'eslint_d' },
+    lua = { 'luacheck' },
     sh = { 'shellcheck' },
     typescript = { 'eslint_d' },
     typescriptreact = { 'eslint_d' },
