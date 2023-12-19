18f2d8f0b15e79fce1616fcca3b9136129585cc8
Oskar Grunning <oskargrunning@gmail.com>
2023-12-19 14:22:42 +0100

    chore: tweak flash config

--- config/nvim/lua/sQVe/plugins/flash.lua
+++ config/nvim/lua/sQVe/plugins/flash.lua
@@ -8,9 +8,9 @@ local M = {
   event = 'VeryLazy',
   keys = {
     -- stylua: ignore start
-    { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash', },
-    { 'S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter() end, desc = 'Flash treesitter', },
-    { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote flash', },
+    { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash search', },
+    { 'S', mode = { 'n', 'x', 'o' }, function() require('flash').treesitter_search() end, desc = 'Flash treesitter search', },
+    { 'r', mode = 'o', function() require('flash').remote() end, desc = 'Remote flash search', },
     -- stylua: ignore end
   },
 }
