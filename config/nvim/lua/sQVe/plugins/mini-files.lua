e79987e3498883f589d23ffb74b70c3cc8d2a31c
Oskar Grunning <oskargrunning@gmail.com>
2023-12-19 13:13:54 +0100

    chore: tweak mini files config

--- config/nvim/lua/sQVe/plugins/mini-files.lua
+++ config/nvim/lua/sQVe/plugins/mini-files.lua
@@ -10,9 +10,16 @@ local M = {
 M.opts = {
   mappings = {
     go_in = '',
-    go_in_plus = '',
+    go_in_plus = '<CR>',
     go_out = '',
-    go_out_plus = '',
+    go_out_plus = '<BS>',
+    reset = '',
+  },
+  windows = {
+    preview = true,
+    width_focus = 40,
+    width_preview = 40,
+    width_nofocus = 20,
   },
 }
 
@@ -28,32 +35,6 @@ M.config = function(_, opts)
   map('n', '_', function()
     mini_files.open(vim.api.nvim_buf_get_name(0), false)
   end, { desc = 'Open file tree at current file' })
-
-  local go_in_plus = function()
-    for _ = 1, vim.v.count1 - 1 do
-      mini_files.go_in()
-    end
-
-    local fs_entry = mini_files.get_fs_entry()
-    local is_at_file = fs_entry ~= nil and fs_entry.fs_type == 'file'
-
-    mini_files.go_in()
-    if is_at_file then
-      mini_files.close()
-    end
-  end
-
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'MiniFilesBufferCreate',
-    callback = function(args)
-      map('n', 'l', go_in_plus, { buffer = args.data.buf_id })
-      map('n', '<CR>', go_in_plus, { buffer = args.data.buf_id })
-      map('n', '<Right>', go_in_plus, { buffer = args.data.buf_id })
-
-      map('n', 'h', mini_files.go_out, { buffer = args.data.buf_id })
-      map('n', '<Left>', mini_files.go_out, { buffer = args.data.buf_id })
-    end,
-  })
 end
 
 return M
