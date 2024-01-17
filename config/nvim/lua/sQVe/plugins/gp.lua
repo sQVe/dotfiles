399e6f2e44486fbce52736826cfefb60e6e2384f
Oskar Grunning <oskargrunning@gmail.com>
2024-01-17 08:24:59 +0100

    fix: refactor prompt on gp nvim

--- config/nvim/lua/sQVe/plugins/gp.lua
+++ config/nvim/lua/sQVe/plugins/gp.lua
@@ -3,11 +3,50 @@
 -- ┗━┛╹
 -- Communicate with the ChatGPT API.
 
+local javascript_filetypes = {
+  'javascript',
+  'javascriptreact',
+  'typescript',
+  'typescriptreact',
+}
+
+-- Ensure that we have access to `table.unpack`.
+if not table.unpack then
+  table.unpack = unpack
+end
+
+--- Generates a prompt string.
+--- @param prompt_lines table<string> The table of lines that will be concatenated to form the prompt.
+--- @vararg string A variable number of arguments used to format the prompt.
+local generate_prompt = function(prompt_lines, ...)
+  local arguments = { ... }
+
+  local format_prompts = function(prompt, arguments)
+    return prompt:format(table.unpack(arguments))
+  end
+
+  -- Ensure that all empty arguments are replaced with '%s', since that allows
+  -- us to sanitize them.
+  for idx, argument in ipairs(arguments) do
+    if argument == '' then
+      arguments[idx] = '%s'
+    end
+  end
+
+  local prompt = table.concat(prompt_lines, '\n')
+  local ok, formatted_prompt = pcall(format_prompts, prompt, arguments)
+
+  -- Remove any left over '%s', including the newline, from the prompt.
+  local sanitized_prompt = (ok and formatted_prompt or prompt):gsub('%%s\n', '')
+
+  return sanitized_prompt
+end
+
 local M = {
   'robitx/gp.nvim',
   keys = {
     -- stylua: ignore start
-    { '<Leader>i*', mode = { 'v' }, ":<C-u>'<,'>GpDocstring<CR>", desc = 'Generate docstring for selection', },
+    { '<Leader>i*', mode = { 'v' }, ":<C-u>'<,'>GpDocumentation<CR>", desc = 'Generate documentation for selection', },
     { '<Leader>i-', mode = { 'v' }, ":<C-u>'<,'>GpCompress<CR>", desc = 'Compress selection', },
     { '<Leader>i=', mode = { 'v' }, ":<C-u>'<,'>GpSummarize<CR>", desc = 'Summarize selection', },
     { '<Leader>ia', mode = { 'v' }, ":<C-u>'<,'>GpAtomic<CR>", desc = 'Suggest note improvements for selection', },
@@ -36,27 +75,35 @@ local agents = {
     chat = true,
     command = false,
     model = { model = 'gpt-4-1106-preview', temperature = 1.1, top_p = 1 },
-    system_prompt = 'You are a general AI assistant.\n\n'
-      .. 'The user has provided additional guidelines for your response:\n\n'
-      .. '- Stay factual: If unsure, admit lack of knowledge rather than guessing.\n'
-      .. '- Seek clarity: When necessary, ask questions to provide better answers.\n'
-      .. '- Foster learning: Utilize the Socratic method to enhance thinking and coding abilities.\n'
-      .. '- Be comprehensive: Include any relevant code in your outputs when required.\n'
-      .. '- Remain calm: Remember to take a deep breath. Confidence is key!',
+    system_prompt = generate_prompt({
+      'You are a general AI assistant.',
+      '',
+      'The user has provided additional guidelines for your response:',
+      '',
+      '- Stay factual: If unsure, admit lack of knowledge rather than guessing.',
+      '- Seek clarity: When necessary, ask questions to provide better answers.',
+      '- Foster learning: Utilize the Socratic method to enhance thinking and coding abilities.',
+      '- Be comprehensive: Include any relevant code in your outputs when required.',
+      '- Remain calm: Remember to take a deep breath. Confidence is key!',
+    }),
   },
   {
     name = 'CodeGPT4',
     chat = false,
     command = true,
-    model = { model = 'gpt-4-1106-preview', temperature = 0.8, top_p = 1 },
-    system_prompt = 'You are an AI working as a code editor.\n\n'
-      .. 'When handling complex concepts or code:\n'
-      .. '- Make sure to INCLUDE EXPLANATORY COMMENTS within the code for better comprehension.\n'
-      .. '- Refrain from adding commentary outside code blocks.\n\n'
-      .. 'Enclose your code-centric answers within code block delimiters:\n'
-      .. '```code\n'
-      .. '-- Your code and comments here\n'
-      .. '```\n',
+    model = { model = 'gpt-4-1106-preview', temperature = 1.1, top_p = 1 },
+    system_prompt = generate_prompt({
+      'You are an AI working as a code editor.',
+      '',
+      'When handling complex concepts or code:',
+      '- Make sure to INCLUDE EXPLANATORY COMMENTS within the code for better comprehension.',
+      '- Refrain from adding commentary outside code blocks.',
+      '',
+      'Enclose your code-centric answers within code block delimiters:',
+      '```code',
+      '-- Your code and comments here',
+      '```',
+    }),
   },
   { name = 'ChatGPT3-5' },
   { name = 'CodeGPT3-5' },
@@ -64,13 +111,19 @@ local agents = {
 
 local hooks = {
   Atomic = function(gp, params)
-    local template = 'I have the following code from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Each note should encompass a single cohesive topic; split the text into distinct, self-contained notes if it spans multiple diverse subjects.\n'
-      .. 'Preserve any YAML data within the notes exactly as it is, ensuring no changes are made to this formatting or content.\n'
-      .. 'Focus on enhancements to grammar and spelling to ensure the notes are well-written and easily comprehensible.\n'
-      .. 'Format and structure each note for optimal clarity, using bullet points, numbered lists, or headings where appropriate.\n'
-      .. 'Please remember that these notes are tailored for personal use and should be optimally organized for quick reference.'
+    local prompt = generate_prompt({
+      'I have the following code from {{filename}}:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Each note should encompass a single cohesive topic; split the text into distinct, self-contained notes if it spans multiple diverse subjects.',
+      'Preserve any YAML data within the notes exactly as it is, ensuring no changes are made to this formatting or content.',
+      'Focus on enhancements to grammar and spelling to ensure the notes are well-written and easily comprehensible.',
+      'Format and structure each note for optimal clarity, using bullet points, numbered lists, or headings where appropriate.',
+      'Please remember that these notes are tailored for personal use and should be optimally organized for quick reference.',
+    })
 
     local agent = gp.get_chat_agent()
 
@@ -79,14 +132,20 @@ local hooks = {
       gp.Target.enew('markdown'),
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Bugs = function(gp, params)
-    local template = 'I have encountered an issue in the code from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Could you help me by identifying and fixing any bugs in the above code?.'
+    local prompt = generate_prompt({
+      'I have encountered an issue in the code from {{filename}}:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Could you help me by identifying and fixing any bugs in the above code?.',
+    })
 
     local agent = gp.get_chat_agent()
 
@@ -95,14 +154,20 @@ local hooks = {
       gp.Target.enew('markdown'),
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Compress = function(gp, params)
-    local template = 'I have the following text from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Could you assist by condensing the text above to its most succinct form?.'
+    local prompt = generate_prompt({
+      'I have the following text from {{filename}}:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Could you assist by condensing the text above to its most succinct form?.',
+    })
 
     local agent = gp.get_command_agent()
 
@@ -111,15 +176,32 @@ local hooks = {
       gp.Target.enew,
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
-  Docstring = function(gp, params)
-    local template = 'I have the following code from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Please provide docstrings for the code block shown above.\n'
-      .. 'Respond with only the docstring snippet, omitting the original code block from your answer..'
+  Documentation = function(gp, params)
+    local current_filetype = vim.bo.filetype
+    local prompt = generate_prompt(
+      {
+        'I have the following code from {{filename}}:',
+        '',
+        '```{{filetype}}',
+        '{{selection}}',
+        '```',
+        '',
+        'Please provide a documentation for the code block above.',
+        '%s',
+        'Keep the wording concise and clear.',
+        'Respond with only the comment snippet, omitting the original code block from your answer.',
+      },
+      (
+        vim.tbl_contains(javascript_filetypes, current_filetype)
+        and 'Ensure that you follow the JSDoc standard for all documentation.'
+      )
+        or (current_filetype == 'lua' and 'Ensure that you use follow the Lua Language Server (`luals`) annotations standard. All docstring comments MUST begin with `---`.')
+        or ''
+    )
 
     local agent = gp.get_command_agent()
 
@@ -128,14 +210,20 @@ local hooks = {
       gp.Target.enew,
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Explain = function(gp, params)
-    local template = 'I need an explanation for the code from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Please provide a detailed explanation of the code block shown above.'
+    local prompt = generate_prompt({
+      'I need an explanation for the code from {{filename}}:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Please provide a detailed explanation of the code block shown above.',
+    })
 
     local agent = gp.get_chat_agent()
 
@@ -144,29 +232,29 @@ local hooks = {
       gp.Target.enew('markdown'),
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Tests = function(gp, params)
-    local template = 'Please devise unit tests for the code excerpted from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Kindly ensure your response exclusively consists of the unit tests corresponding to the presented code snippet.'
-
     local current_filetype = vim.bo.filetype
-    local javascript_filetypes = {
-      'javascript',
-      'javascriptreact',
-      'typescript',
-      'typescriptreact',
-    }
+    local prompt = generate_prompt(
+      {
+        'Please write unit tests for the code from {{filename}}:',
+        '',
+        '```{{filetype}}',
+        '{{selection}}',
+        '```',
+        '',
+        '%s',
+        'Ensure your response exclusively consists of the unit tests corresponding to the presented code snippet.',
+      },
 
-    if vim.tbl_contains(javascript_filetypes, current_filetype) then
-      template = template
-        .. '\nPlease use `vitest` as the test runner and `@testing-library` for any DOM-related tests.'
-    end
-
-    template = template .. '\nRespond exclusively with the snippet.'
+      (
+        vim.tbl_contains(javascript_filetypes, current_filetype)
+        and 'Please use `vitest` as the test runner and `@testing-library` for any DOM-related tests.'
+      ) or ''
+    )
 
     local agent = gp.get_command_agent()
 
@@ -175,15 +263,21 @@ local hooks = {
       gp.Target.enew,
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Optimize = function(gp, params)
-    local template = 'The code from {{filename}} requires optimization:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Please examine the code for any code smells and suggest improvements.\n'
-      .. 'Your response should focus on optimizing the provided code snippet.'
+    local prompt = generate_prompt({
+      'The code from {{filename}} requires optimization:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Please examine the code for any code smells and suggest improvements.',
+      'Your response should focus on optimizing the provided code snippet.',
+    })
 
     local agent = gp.get_chat_agent()
 
@@ -192,14 +286,20 @@ local hooks = {
       gp.Target.enew('markdown'),
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Readability = function(gp, params)
-    local template = 'A more readable version is requested for the text from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Please enhance the clarity and readability of the code snippet above in your response.'
+    local prompt = generate_prompt({
+      'Give me a more readable version of the text from {{filename}}:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Please enhance the clarity and readability of the code snippet above in your response.',
+    })
 
     local agent = gp.get_chat_agent()
 
@@ -208,14 +308,20 @@ local hooks = {
       gp.Target.enew('markdown'),
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Spelling = function(gp, params)
-    local template = 'Correct the grammar and spelling in the following text from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Please respond with the revised text only.'
+    local prompt = generate_prompt({
+      'Correct the grammar and spelling in the following text from {{filename}}:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Please respond with the revised text only.',
+    })
 
     local agent = gp.get_command_agent()
 
@@ -224,14 +330,20 @@ local hooks = {
       gp.Target.enew,
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
   Summarize = function(gp, params)
-    local template = 'Summarize the text from {{filename}}:\n\n'
-      .. '```{{filetype}}\n{{selection}}\n```\n\n'
-      .. 'Please focus your response on a concise summary of the provided text.'
+    local prompt = generate_prompt({
+      'Summarize the text from {{filename}}:',
+      '',
+      '```{{filetype}}',
+      '{{selection}}',
+      '```',
+      '',
+      'Please focus your response on a concise summary of the provided text.',
+    })
 
     local agent = gp.get_chat_agent()
 
@@ -240,7 +352,7 @@ local hooks = {
       gp.Target.enew('markdown'),
       nil,
       agent.model,
-      template,
+      prompt,
       agent.system_prompt
     )
   end,
