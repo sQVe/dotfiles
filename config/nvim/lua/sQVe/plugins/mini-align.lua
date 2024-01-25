b1a1ad84c480eb6a8de9dbd09284e56d10db7855
Oskar Grunning <oskargrunning@gmail.com>
2024-01-25 11:50:11 +0100

    fix: add mini.align plugin

---
+++ config/nvim/lua/sQVe/plugins/mini-align.lua
@@ -0,0 +1,25 @@
+-- ┏┳┓╻┏┓╻╻   ┏━┓╻  ╻┏━╸┏┓╻
+-- ┃┃┃┃┃┗┫┃   ┣━┫┃  ┃┃╺┓┃┗┫
+-- ╹ ╹╹╹ ╹╹   ╹ ╹┗━╸╹┗━┛╹ ╹
+-- Align text
+
+local M = {
+  'echasnovski/mini.align',
+  keys = {
+    { 'ga', mode = { 'n', 'v' }, desc = 'Align' },
+  },
+}
+
+M.opts = {
+  mappings = {
+    start = '',
+    start_with_preview = 'ga',
+  },
+  options = {
+    split_pattern = '',
+    justify_side = 'left',
+    merge_delimiter = '',
+  },
+}
+
+return M
