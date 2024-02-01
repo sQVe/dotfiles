5e6e6a3edc4f04585d69df3646871a74dbdc9ee3
Oskar Grunning <oskargrunning@gmail.com>
2024-02-01 07:07:42 +0100

    fix: repair indenting on markdown files

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -8,6 +8,10 @@ local augroup = require('sQVe.utils.vim').augroup
 -- Enable conceal.
 vim.opt_local.conceallevel = 2
 
+-- Indent with two spaces.
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+
 -- Enable spellchecking for notes.
 autocmd({ 'BufEnter' }, {
   group = augroup('SetNoteSpellcheck'),
