6947a62b864df47ef7831f1bc294f77adaff3e8a
Oskar Grunning <oskargrunning@gmail.com>
2024-03-22 15:49:55 +0100

    refactor: tweak grapple

--- config/nvim/lua/sQVe/plugins/grapple.lua
+++ config/nvim/lua/sQVe/plugins/grapple.lua
@@ -8,41 +8,59 @@ local M = {
   event = { 'BufReadPost', 'BufNewFile' },
   cmd = 'Grapple',
   keys = {
-    { '<Leader>g', '<Cmd>Grapple toggle<CR>', desc = 'Grapple toggle tag' },
     {
-      '<Backspace>',
+      '<Leader>j',
+      function()
+        local grapple = require('grapple')
+
+        if grapple.exists() then
+          grapple.untag({ path = vim.fn.expand('%:p') })
+          vim.notify(
+            string.format('Untagged file %s.', vim.fn.expand('%:t')),
+            vim.log.levels.INFO
+          )
+        else
+          grapple.tag({
+            -- Set tag name to the file name without the extension.
+            name = vim.fn.expand('%:t:r'),
+            vim.notify(
+              string.format('Tagged file %s.', vim.fn.expand('%:t')),
+              vim.log.levels.INFO
+            ),
+          })
+        end
+      end,
+      desc = 'Grapple toggle tag',
+    },
+    {
+      '<Leader><Backspace>',
       '<Cmd>Grapple toggle_tags<CR>',
       desc = 'Grapple toggle tags',
     },
     {
-      '<Tab>',
-      '<Cmd>Grapple cycle forward<CR>',
-      desc = 'Grapple cycle forward',
-    },
-    {
-      '<S-Tab>',
-      '<Cmd>Grapple cycle backward<CR>',
-      desc = 'Grapple cycle backward',
-    },
-    {
       '<Leader>1',
       '<Cmd>Grapple select index=1<CR>',
-      desc = 'Grapple select 1',
+      desc = 'Go-to tag 1',
     },
     {
       '<Leader>2',
       '<Cmd>Grapple select index=2<CR>',
-      desc = 'Grapple select 2',
+      desc = 'Go-to tag 2',
     },
     {
       '<Leader>3',
       '<Cmd>Grapple select index=3<CR>',
-      desc = 'Grapple select 3',
+      desc = 'Go-to tag 3',
     },
     {
       '<Leader>4',
       '<Cmd>Grapple select index=4<CR>',
-      desc = 'Grapple select 4',
+      desc = 'Go-to tag 4',
+    },
+    {
+      '<Leader>5',
+      '<Cmd>Grapple select index=5<CR>',
+      desc = 'Go-to tag 5',
     },
   },
 }
