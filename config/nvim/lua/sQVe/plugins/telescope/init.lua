53cc5fbe1cba7cfb54c12775fa9e9455493164e5
Oskar Grunning <oskargrunning@gmail.com>
2024-04-25 13:46:03 +0200

    Revert "chore: tweak telescope setup"
    This reverts commit dc53dfc76b77d67f6a7a51e5431b623d2c0383a2.

--- config/nvim/lua/sQVe/plugins/telescope/init.lua
+++ config/nvim/lua/sQVe/plugins/telescope/init.lua
@@ -10,8 +10,8 @@ local M = {
   'nvim-telescope/telescope.nvim',
   cmd = 'Telescope',
   dependencies = {
-    'natecraddock/telescope-zf-native.nvim',
-    'debugloop/telescope-undo.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'debugloop/telescope-undo.nvim' },
   },
   keys = {
     {
@@ -130,9 +130,10 @@ M.opts = function()
       },
     },
     extensions = {
-      ['zf-native'] = {
-        file = { enable = true, smart_case = false },
-        generic = { enable = false },
+      fzf = {
+        case_mode = 'respect_case',
+        override_file_sorter = true,
+        override_generic_sorter = false,
       },
       undo = {
         side_by_side = true,
@@ -163,8 +164,8 @@ M.config = function(_, opts)
   local telescope = require('telescope')
 
   telescope.setup(opts)
+  telescope.load_extension('fzf')
   telescope.load_extension('undo')
-  telescope.load_extension('zf-native')
 end
 
 return M
