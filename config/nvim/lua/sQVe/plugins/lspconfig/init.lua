cc0df48d2b59ceb6e267f92251076d34ddcfcba2
Oskar Grunning <oskargrunning@gmail.com>
2024-04-27 17:16:34 +0200

    chore: update neovim

--- config/nvim/lua/sQVe/plugins/lspconfig/init.lua
+++ config/nvim/lua/sQVe/plugins/lspconfig/init.lua
@@ -72,10 +72,10 @@ M.config = function()
         -- Use a specific Node.js version that is compatible with
         -- cucumber-language-server.
         vim.fn.expand('$NVM_DIR')
-          .. '/versions/node/v18.20.1/bin'
+          .. '/versions/node/v18.20.2/bin'
           .. '/node',
         vim.fn.expand('$NVM_DIR')
-          .. '/versions/node/v18.20.1/bin'
+          .. '/versions/node/v18.20.2/bin'
           .. '/cucumber-language-server',
         '--stdio',
       },
