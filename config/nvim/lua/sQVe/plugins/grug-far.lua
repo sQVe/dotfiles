2d7ccac240b871e7fbe643865bfb435919ff7225
Oskar Grunning <oskargrunning@gmail.com>
2024-05-30 11:21:08 +0200

    chore: set grug-far bindings

--- config/nvim/lua/sQVe/plugins/grug-far.lua
+++ config/nvim/lua/sQVe/plugins/grug-far.lua
@@ -10,8 +10,16 @@ local M = {
 
 M.opts = {
   keymaps = {
-    syncLocations = { n = '=' },
+    close = { n = '<Leader>q' },
+    gotoLocation = { n = '<CR>' },
+    historyAdd = { n = '' },
+    historyOpen = { n = '<C-p>' },
+    pickHistoryEntry = { n = '<CR>' },
+    qflist = { n = '<C-q>' },
+    refresh = { n = '<C-r>' },
+    replace = { n = '<C-CR>' },
     syncLine = { n = '-' },
+    syncLocations = { n = '=' },
   },
 }
 
