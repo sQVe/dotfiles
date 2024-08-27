9905e4218313ffbd72e742595a4acc24e5da040b
Oskar Grunning <oskargrunning@gmail.com>
2024-08-27 11:46:19 +0200

    chore: fix codecompanion deprecations

--- config/nvim/lua/sQVe/plugins/codecompanion/adapters.lua
+++ config/nvim/lua/sQVe/plugins/codecompanion/adapters.lua
@@ -7,7 +7,7 @@ local M = {}
 M.openai = function()
   local adapters = require('codecompanion.adapters')
 
-  return adapters.use('openai', {
+  return adapters.extend('openai', {
     schema = {
       temperature = { default = 0.6 },
       top_p = { default = 0.4 },
