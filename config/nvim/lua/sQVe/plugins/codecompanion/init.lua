674a88bcde34a7a63649114ae36c49a4710be520
Oskar Grunning <oskargrunning@gmail.com>
2024-09-30 09:07:38 +0200

    fix: refactor codecompanion

--- config/nvim/lua/sQVe/plugins/codecompanion/init.lua
+++ config/nvim/lua/sQVe/plugins/codecompanion/init.lua
@@ -6,7 +6,7 @@
 local format_lines = require('sQVe.utils.format_lines')
 local autocmd = require('sQVe.utils.autocmd')
 
-local actions = require('sQVe.plugins.codecompanion.actions')
+local prompts = require('sQVe.plugins.codecompanion.prompts')
 local adapters = require('sQVe.plugins.codecompanion.adapters')
 local ui = require('sQVe.plugins.codecompanion.ui')
 
@@ -24,65 +24,73 @@ local M = {
   },
 }
 
-M.opts = function()
-  return {
-    actions = {
-      actions.chat(),
-      actions.condense_text(),
-      actions.debug_code_diagnostics(),
-      actions.enhance_text_readability(),
-      actions.explain_code(),
-      actions.generate_docstring(),
-      actions.generate_pull_request_description(),
-      actions.generate_unit_tests(),
-      actions.improve_code(),
-      actions.proofread_text(),
-      actions.rephrase_text(),
-      actions.write_commit_message(),
-    },
-    adapters = {
-      openai = adapters.openai,
-    },
-    display = {
-      action_palette = { width = 80, height = 10, relative = 'editor' },
-      chat = {
-        intro_message = '',
-        show_settings = true,
-        window = {
-          layout = 'buffer',
-          opts = { cursorcolumn = false, cursorline = false },
-        },
+M.opts = {
+  adapters = {
+    openai = adapters.openai,
+  },
+  display = {
+    action_palette = {
+      width = 80,
+      height = 10,
+      relative = 'editor',
+      opts = {
+        show_default_actions = false,
+        show_default_prompt_library = false,
       },
     },
-    opts = {
-      system_prompt = format_lines({
-        "You are an AI programming assistant. You're plugged into the Neovim text editor on a user's machine.",
-        '',
-        'You must:',
-        "- Follow the user's requirements carefully and to the letter.",
-        '- Keep your answers short and impersonal, especially if the user responds with context outside of your task',
-        '- Minimize prose.',
-        '- Use Markdown formatting in your answers.',
-        '- Include the programming language name at the start of the Markdown code blocks.',
-        '- Avoid line numbers in code blocks.',
-        '- Avoid wrapping the whole response in triple backticks.',
-        '- Only return relevant code.',
-        '',
-        'When given a task:',
-        '1. Think step-by-step and describe your plan for what to build in pseudocode, written out in great detail.',
-        '2. Output the code in a single code block.',
-        '3. You can only give one reply for each.',
-      }),
-      use_default_actions = false,
-      use_default_prompts = false,
-    },
-    strategies = {
-      chat = {
-        roles = { llm = 'Assistant', user = 'User' },
+    chat = {
+      intro_message = '',
+      show_settings = true,
+      window = {
+        layout = 'buffer',
+        opts = { cursorcolumn = false, cursorline = false },
       },
     },
-  }
-end
+  },
+  opts = {
+    system_prompt = format_lines({
+      "You are an AI programming assistant. You're plugged into the Neovim text editor on a user's machine.",
+      '',
+      'You must:',
+      "- Follow the user's requirements carefully and to the letter.",
+      '- Keep your answers short and impersonal, especially if the user responds with context outside of your task',
+      '- Minimize prose.',
+      '- Use Markdown formatting in your answers.',
+      '- Include the programming language name at the start of the Markdown code blocks.',
+      '- Avoid line numbers in code blocks.',
+      '- Avoid wrapping the whole response in triple backticks.',
+      '- Only return relevant code.',
+      '',
+      'When given a task:',
+      '1. Think step-by-step and describe your plan for what to build in pseudocode, written out in great detail.',
+      '2. Output the code in a single code block.',
+      '3. You can only give one reply for each.',
+    }),
+  },
+  prompt_library = {
+    ['Chat'] = prompts.chat,
+    ['Condense text'] = prompts.condense_text,
+    ['Debug code diagnostics'] = prompts.debug_code_diagnostics,
+    ['Enhance text readability'] = prompts.enhance_text_readability,
+    ['Explain code'] = prompts.explain_code,
+    ['Generate docstring'] = prompts.generate_docstring,
+    ['Generate pull request description'] = prompts.generate_pull_request_description,
+    ['Generate unit tests'] = prompts.generate_unit_tests,
+    ['Improve code'] = prompts.improve_code,
+    ['Proofread text'] = prompts.proofread_text,
+    ['Rephrase text'] = prompts.rephrase_text,
+    ['Write commit message'] = prompts.write_commit_message,
+  },
+  strategies = {
+    chat = {
+      adapter = 'openai',
+      roles = {
+        llm = 'Assistant',
+        user = 'User',
+      },
+    },
+  },
+}
 
 M.config = function(_, opts)
   ui.override()
