58f2d4c9788cb952a825016bc15118bc60e83267
Oskar Grunning <oskargrunning@gmail.com>
2025-01-07 13:25:54 +0100

    fix: update blink etc

--- config/nvim/lua/sQVe/plugins/blink.lua
+++ config/nvim/lua/sQVe/plugins/blink.lua
@@ -7,98 +7,118 @@ local M = {
   'saghen/blink.cmp',
   build = 'cargo build --release',
   event = 'InsertEnter',
+  dependencies = { 'xzbdmw/colorful-menu.nvim' },
 }
 
-M.opts = {
-  completion = {
-    list = { selection = 'auto_insert' },
-    menu = {
-      draw = {
-        columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon' } },
-      },
-    },
-    documentation = { auto_show = true },
-  },
-  keymap = {
-    ['<C-Space>'] = { 'hide_documentation', 'show', 'show_documentation' },
-    ['<C-CR>'] = { 'hide_documentation', 'show', 'show_documentation' },
-    ['<C-e>'] = { 'hide', 'fallback' },
-    ['<CR>'] = { 'accept', 'fallback' },
-    ['<Tab>'] = { 'select_next', 'fallback' },
-    ['<S-Tab>'] = { 'select_prev', 'fallback' },
-    ['<Down>'] = { 'select_next', 'fallback' },
-    ['<Up>'] = { 'select_prev', 'fallback' },
-    ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
-    ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
-    ['<C-j>'] = { 'snippet_forward', 'fallback' },
-    ['<C-k>'] = { 'snippet_backward', 'fallback' },
-  },
-  signature = { enabled = true },
-  sources = {
-    default = function()
-      local node = vim.treesitter.get_node()
-      local enabled_providers = { 'lsp', 'buffer', 'path', 'snippets' }
-
-      if
-        node
-        and vim.tbl_contains({
-          'comment',
-          'line_comment',
-          'block_comment',
-        }, node:type())
-      then
-        return { 'buffer', 'path' }
-      end
-
-      if vim.bo.filetype == 'lua' then
-        table.insert(enabled_providers, 1, 'luasnip')
-      end
-
-      return enabled_providers
-    end,
-
-    providers = {
-      lazydev = {
-        name = 'LazyDev',
-        module = 'lazydev.integrations.blink',
-        fallbacks = { 'lsp' },
-      },
-      lsp = {
-        name = 'LSP',
-        module = 'blink.cmp.sources.lsp',
-      },
-      buffer = {
-        name = 'Buffer',
-        module = 'blink.cmp.sources.buffer',
-      },
-      path = {
-        name = 'Path',
-        module = 'blink.cmp.sources.path',
-        score_offset = 3,
-        opts = {
-          show_hidden_files_by_default = true,
-        },
-      },
-      snippets = {
-        name = 'Snippets',
-        module = 'blink.cmp.sources.snippets',
-        min_keyword_length = 1,
-        score_offset = -3,
-        opts = {
-          extended_filetypes = {
-            javascriptreact = { 'javascript' },
-            typescript = { 'javascript' },
-            typescriptreact = {
-              'javascript',
-              'javascriptreact',
-              'typescript',
+M.opts = function()
+  return {
+    completion = {
+      list = { selection = { auto_insert = true, preselect = false } },
+      menu = {
+        draw = {
+          columns = { { 'label', gap = 1 }, { 'kind_icon' } },
+          components = {
+            label = {
+              text = require('colorful-menu').blink_components_text,
+              highlight = require('colorful-menu').blink_components_highlight,
+            },
+            kind_icon = {
+              ellipsis = false,
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+              text = function(ctx)
+                local icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                return icon
+              end,
             },
           },
-          friendly_snippets = false,
+        },
+      },
+      documentation = { auto_show = true },
+    },
+    keymap = {
+      ['<C-Space>'] = { 'hide_documentation', 'show', 'show_documentation' },
+      ['<C-CR>'] = { 'hide_documentation', 'show', 'show_documentation' },
+      ['<C-e>'] = { 'hide', 'fallback' },
+      ['<CR>'] = { 'accept', 'fallback' },
+      ['<Tab>'] = { 'select_next', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<C-u>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-d>'] = { 'scroll_documentation_down', 'fallback' },
+      ['<C-j>'] = { 'snippet_forward', 'fallback' },
+      ['<C-k>'] = { 'snippet_backward', 'fallback' },
+    },
+    signature = { enabled = true },
+    sources = {
+      default = function()
+        local node = vim.treesitter.get_node()
+        local enabled_providers = { 'lsp', 'buffer', 'path', 'snippets' }
+
+        if
+          node
+          and vim.tbl_contains({
+            'comment',
+            'line_comment',
+            'block_comment',
+          }, node:type())
+        then
+          return { 'buffer', 'path' }
+        end
+
+        if vim.bo.filetype == 'lua' then
+          table.insert(enabled_providers, 1, 'luasnip')
+        end
+
+        return enabled_providers
+      end,
+
+      providers = {
+        lazydev = {
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
+          fallbacks = { 'lsp' },
+        },
+        lsp = {
+          name = 'LSP',
+          module = 'blink.cmp.sources.lsp',
+        },
+        buffer = {
+          name = 'Buffer',
+          module = 'blink.cmp.sources.buffer',
+        },
+        path = {
+          name = 'Path',
+          module = 'blink.cmp.sources.path',
+          score_offset = 3,
+          opts = {
+            show_hidden_files_by_default = true,
+          },
+        },
+        snippets = {
+          name = 'Snippets',
+          module = 'blink.cmp.sources.snippets',
+          min_keyword_length = 1,
+          score_offset = -3,
+          opts = {
+            extended_filetypes = {
+              javascriptreact = { 'javascript' },
+              typescript = { 'javascript' },
+              typescriptreact = {
+                'javascript',
+                'javascriptreact',
+                'typescript',
+              },
+            },
+            friendly_snippets = false,
+          },
         },
       },
     },
-  },
-}
+  }
+end
 
 return M
