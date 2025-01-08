6a7223916501835b6d96f3875ddc31c48e3cf2a7
Oskar Grunning <oskargrunning@gmail.com>
2025-01-08 11:32:57 +0100

    chore: improve lazydev

--- config/nvim/lua/sQVe/plugins/lazydev.lua
+++ config/nvim/lua/sQVe/plugins/lazydev.lua
@@ -6,15 +6,11 @@
 local M = {
   'folke/lazydev.nvim',
   ft = 'lua',
-  dependencies = {
-    { 'Bilal2453/luvit-meta', lazy = true },
-  },
 }
 
 M.opts = {
   library = {
-    { path = 'LazyVim', words = { 'LazyVim' } },
-    { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+    { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
   },
 }
 
