337b8ded80db479d7f5a4dc06b0698729e092a8e
Oskar Grunning <oskargrunning@gmail.com>
2025-04-17 10:00:02 +0200

    fix: move to mini cursorword from illuminate

---
+++ config/nvim/lua/sQVe/plugins/mini-cursorword.lua
@@ -0,0 +1,13 @@
+-- ┏┳┓╻┏┓╻╻   ┏━╸╻ ╻┏━┓┏━┓┏━┓┏━┓╻ ╻┏━┓┏━┓╺┳┓
+-- ┃┃┃┃┃┗┫┃   ┃  ┃ ┃┣┳┛┗━┓┃ ┃┣┳┛┃╻┃┃ ┃┣┳┛ ┃┃
+-- ╹ ╹╹╹ ╹╹   ┗━╸┗━┛╹┗╸┗━┛┗━┛╹┗╸┗┻┛┗━┛╹┗╸╺┻┛
+-- Highlight the word under the cursor.
+
+local M = {
+  'echasnovski/mini.cursorword',
+  event = 'VeryLazy',
+}
+
+M.opts = { delay = 200 }
+
+return M
