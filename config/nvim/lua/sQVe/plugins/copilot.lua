1a2d52d13b7307ee2bf0f639d711724529928189
Oskar Grunning <oskargrunning@gmail.com>
2025-09-28 20:28:42 +0200

    chore: refactor copilot setup

--- config/nvim/lua/sQVe/plugins/copilot.lua
+++ config/nvim/lua/sQVe/plugins/copilot.lua
@@ -28,20 +28,6 @@ M.config = function(_, opts)
 
   require('copilot').setup(opts)
 
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'BlinkCmpMenuOpen',
-    callback = function()
-      vim.b.copilot_suggestion_hidden = true
-    end,
-  })
-
-  vim.api.nvim_create_autocmd('User', {
-    pattern = 'BlinkCmpMenuClose',
-    callback = function()
-      vim.b.copilot_suggestion_hidden = false
-    end,
-  })
-
   local suggestion = require('copilot.suggestion')
 
   map('i', '<C-l>', function()
