3fa25ac53e1da2f43d3e1b413a2b8521b099ce52
Oskar Grunning <oskargrunning@gmail.com>
2026-01-15 17:14:09 +0100

    feat(nvim): show git repo root and branch in lualine
    Replaces grapple indicator with branch name. Adds git root
    name to identify which repository is active in worktrees.

--- config/nvim/lua/sQVe/plugins/lualine.lua
+++ config/nvim/lua/sQVe/plugins/lualine.lua
@@ -19,6 +19,20 @@ M.init = function()
   end
 end
 
+local git_root_name = function()
+  local git_dir = vim.fn.systemlist('git rev-parse --git-common-dir')[1]
+  if vim.v.shell_error ~= 0 or git_dir == nil then
+    return ''
+  end
+  -- For worktrees, git-common-dir returns absolute path to .git
+  -- For regular repos, it returns relative ".git"
+  if git_dir == '.git' then
+    local root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+    return vim.fn.fnamemodify(root, ':t')
+  end
+  return vim.fn.fnamemodify(git_dir, ':h:t')
+end
+
 M.opts = function()
   local palette = require('sQVe.plugins.catppuccin.palette')
 
@@ -32,9 +46,9 @@ M.opts = function()
     },
     sections = {
       lualine_a = {},
-      lualine_b = {},
+      lualine_b = { { git_root_name } },
       lualine_c = {
-        { 'grapple', color = { fg = palette.colors.gruvbox.gray } },
+        { 'branch', color = { fg = palette.colors.gruvbox.gray } },
       },
       lualine_x = {
         {
